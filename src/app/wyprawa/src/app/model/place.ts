5e25d96f87479d2955348e147090e68972e095ae
banan314 <klopusz492@outlook.com>
2017-05-19 20:22:37 +0200

    dashboard with nav bar and google map, bootstrapped

---
+++ src/app/wyprawa/src/app/model/place.ts
@@ -0,0 +1,7 @@
+export class Place {
+  id: number;
+  firstName: string;
+  lastName: string;
+  age: number;
+  hobbys: string[];
+}
---
+++ src/app/wyprawa/src/app/model/user.ts
@@ -0,0 +1,8 @@
+export class User {
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+  visited: boolean;
+  description: string;
+}
